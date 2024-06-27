d7a71619c4414c1c23ec50efba585253a91aed4e
isabla <blais.isael.entr@gmail.com>
2024-06-27 10:50:23 -0400

    Code format with prettier

--- lib/src/global-variable-adapter.ts
+++ lib/src/global-variable-adapter.ts
@@ -1,47 +1,54 @@
-import { createLocalStorageAdapter, LocalStorageAdapter } from './local-storage-adapter';
+import {
+  createLocalStorageAdapter,
+  LocalStorageAdapter
+} from './local-storage-adapter';
 
 declare global {
-    interface Window {
-        __queueThat__?: { [key: string]: string };
-        attachEvent(event: string, listener: EventListener): boolean;
-        detachEvent(event: string, listener: EventListener): void;
-    }
+  interface Window {
+    __queueThat__?: { [key: string]: string };
+    attachEvent(event: string, listener: EventListener): boolean;
+    detachEvent(event: string, listener: EventListener): void;
+  }
 }
 
 export interface GlobalVariableAdapter extends LocalStorageAdapter {
-    save: (key: string, data: string) => void;
-    load: (key: string) => string | null;
-    remove: (key: string) => void;
-    //type: string;
+  save: (key: string, data: string) => void;
+  load: (key: string) => string | null;
+  remove: (key: string) => void;
+  //type: string;
 }
 
-export function createGlobalVariableAdapter(queueName: string): GlobalVariableAdapter {
-    window.__queueThat__ = window.__queueThat__ || {};
+export function createGlobalVariableAdapter(
+  queueName: string
+): GlobalVariableAdapter {
+  window.__queueThat__ = window.__queueThat__ || {};
 
-    const localStorageAdapter = createLocalStorageAdapter(queueName) as GlobalVariableAdapter;
-    localStorageAdapter.save = save;
-    localStorageAdapter.load = load;
-    localStorageAdapter.remove = remove;
-    localStorageAdapter.type = 'globalVariable';
+  const localStorageAdapter = createLocalStorageAdapter(
+    queueName
+  ) as GlobalVariableAdapter;
+  localStorageAdapter.save = save;
+  localStorageAdapter.load = load;
+  localStorageAdapter.remove = remove;
+  localStorageAdapter.type = 'globalVariable';
 
-    return localStorageAdapter;
+  return localStorageAdapter;
 
-    function save(key: string, data: string) {
-        if (window.__queueThat__) {
-            window.__queueThat__[key] = String(data);
-        }
+  function save(key: string, data: string) {
+    if (window.__queueThat__) {
+      window.__queueThat__[key] = String(data);
     }
+  }
 
-    function load(key: string): string | null {
-        if (window.__queueThat__) {
-            return window.__queueThat__[key];
-        }
-        return null;
+  function load(key: string): string | null {
+    if (window.__queueThat__) {
+      return window.__queueThat__[key];
     }
+    return null;
+  }
 
-    function remove(key: string) {
-        if (window.__queueThat__) {
-            delete window.__queueThat__[key];
-        }
+  function remove(key: string) {
+    if (window.__queueThat__) {
+      delete window.__queueThat__[key];
     }
+  }
 }
--- lib/src/local-storage-adapter.ts
+++ lib/src/local-storage-adapter.ts
@@ -5,161 +5,163 @@ const ERROR_COUNT_KEY = '* - Error Count';
 const QUEUE_PROCESSING_KEY = '* - Queue Processing';
 
 interface QueueItem {
-    id: number;
-    ts: number;
+  id: number;
+  ts: number;
 }
 
 export interface LocalStorageAdapter {
-    getQueue: () => QueueItem[];
-    setQueue: (queue: QueueItem[]) => void;
-    getErrorCount: () => number;
-    getBackoffTime: () => number;
-    setErrorCount: (n: number) => void;
-    setBackoffTime: (n: number) => void;
-    getActiveQueue: () => QueueItem | null;
-    setActiveQueue: (id: number) => void;
-    clearActiveQueue: () => void;
-    getQueueProcessing: () => boolean;
-    setQueueProcessing: (isProcessing: boolean) => void;
-    save: (key: string, data: string) => void;
-    load: (key: string) => string | null;
-    works: () => boolean;
-    reset: () => void;
-    remove: (key: string) => void;
-    type: string;
-    flush: () => void;
+  getQueue: () => QueueItem[];
+  setQueue: (queue: QueueItem[]) => void;
+  getErrorCount: () => number;
+  getBackoffTime: () => number;
+  setErrorCount: (n: number) => void;
+  setBackoffTime: (n: number) => void;
+  getActiveQueue: () => QueueItem | null;
+  setActiveQueue: (id: number) => void;
+  clearActiveQueue: () => void;
+  getQueueProcessing: () => boolean;
+  setQueueProcessing: (isProcessing: boolean) => void;
+  save: (key: string, data: string) => void;
+  load: (key: string) => string | null;
+  works: () => boolean;
+  reset: () => void;
+  remove: (key: string) => void;
+  type: string;
+  flush: () => void;
 }
 
-export function createLocalStorageAdapter(queueName: string): LocalStorageAdapter {
-    const queueKey = QUEUE_KEY.replace('*', queueName);
-    const activeQueueKey = ACTIVE_QUEUE_KEY.replace('*', queueName);
-    const backoffTimeKey = BACKOFF_TIME_KEY.replace('*', queueName);
-    const errorCountKey = ERROR_COUNT_KEY.replace('*', queueName);
-    const queueProcessingKey = QUEUE_PROCESSING_KEY.replace('*', queueName);
+export function createLocalStorageAdapter(
+  queueName: string
+): LocalStorageAdapter {
+  const queueKey = QUEUE_KEY.replace('*', queueName);
+  const activeQueueKey = ACTIVE_QUEUE_KEY.replace('*', queueName);
+  const backoffTimeKey = BACKOFF_TIME_KEY.replace('*', queueName);
+  const errorCountKey = ERROR_COUNT_KEY.replace('*', queueName);
+  const queueProcessingKey = QUEUE_PROCESSING_KEY.replace('*', queueName);
 
-    let dirtyCache = true;
-    let setPending = false;
-    let queueCache: QueueItem[] = [];
+  let dirtyCache = true;
+  let setPending = false;
+  let queueCache: QueueItem[] = [];
 
-    const adapter: LocalStorageAdapter = {
-        getQueue,
-        setQueue,
-        getErrorCount,
-        getBackoffTime,
-        setErrorCount,
-        setBackoffTime,
-        getActiveQueue,
-        setActiveQueue,
-        clearActiveQueue,
-        getQueueProcessing,
-        setQueueProcessing,
-        save,
-        load,
-        works,
-        reset,
-        remove,
-        type: 'localStorage',
-        flush
-    };
+  const adapter: LocalStorageAdapter = {
+    getQueue,
+    setQueue,
+    getErrorCount,
+    getBackoffTime,
+    setErrorCount,
+    setBackoffTime,
+    getActiveQueue,
+    setActiveQueue,
+    clearActiveQueue,
+    getQueueProcessing,
+    setQueueProcessing,
+    save,
+    load,
+    works,
+    reset,
+    remove,
+    type: 'localStorage',
+    flush
+  };
 
-    return adapter;
+  return adapter;
 
-    function flush() {
-        dirtyCache = true;
-        if (setPending) {
-            adapter.save(queueKey, JSON.stringify(queueCache));
-            setPending = false;
-        }
+  function flush() {
+    dirtyCache = true;
+    if (setPending) {
+      adapter.save(queueKey, JSON.stringify(queueCache));
+      setPending = false;
     }
+  }
 
-    function getQueue(): QueueItem[] {
-        if (dirtyCache) {
-            queueCache = JSON.parse(adapter.load(queueKey) || '[]');
-            dirtyCache = false;
-            setTimeout(flush, 0);
-        }
-        return queueCache;
+  function getQueue(): QueueItem[] {
+    if (dirtyCache) {
+      queueCache = JSON.parse(adapter.load(queueKey) || '[]');
+      dirtyCache = false;
+      setTimeout(flush, 0);
     }
+    return queueCache;
+  }
 
-    function setQueue(queue: QueueItem[]) {
-        queueCache = queue;
-        dirtyCache = false;
-        setPending = true;
-        setTimeout(flush, 0);
-    }
+  function setQueue(queue: QueueItem[]) {
+    queueCache = queue;
+    dirtyCache = false;
+    setPending = true;
+    setTimeout(flush, 0);
+  }
 
-    function getErrorCount(): number {
-        const count = adapter.load(errorCountKey);
-        return count === null ? 0 : Number(count);
-    }
+  function getErrorCount(): number {
+    const count = adapter.load(errorCountKey);
+    return count === null ? 0 : Number(count);
+  }
 
-    function getBackoffTime(): number {
-        const time = adapter.load(backoffTimeKey);
-        return time === null ? 0 : Number(time);
-    }
+  function getBackoffTime(): number {
+    const time = adapter.load(backoffTimeKey);
+    return time === null ? 0 : Number(time);
+  }
 
-    function setErrorCount(n: number) {
-        adapter.save(errorCountKey, n.toString());
-    }
+  function setErrorCount(n: number) {
+    adapter.save(errorCountKey, n.toString());
+  }
 
-    function setBackoffTime(n: number) {
-        adapter.save(backoffTimeKey, n.toString());
-    }
+  function setBackoffTime(n: number) {
+    adapter.save(backoffTimeKey, n.toString());
+  }
 
-    function getActiveQueue(): QueueItem | null {
-        const activeQueue = adapter.load(activeQueueKey);
-        return activeQueue === null ? null : JSON.parse(activeQueue);
-    }
+  function getActiveQueue(): QueueItem | null {
+    const activeQueue = adapter.load(activeQueueKey);
+    return activeQueue === null ? null : JSON.parse(activeQueue);
+  }
 
-    function setActiveQueue(id: number) {
-        adapter.save(activeQueueKey, JSON.stringify({ id, ts: now() }));
-    }
+  function setActiveQueue(id: number) {
+    adapter.save(activeQueueKey, JSON.stringify({ id, ts: now() }));
+  }
 
-    function clearActiveQueue() {
-        adapter.remove(activeQueueKey);
-    }
+  function clearActiveQueue() {
+    adapter.remove(activeQueueKey);
+  }
 
-    function getQueueProcessing(): boolean {
-        return Boolean(Number(adapter.load(queueProcessingKey)));
-    }
+  function getQueueProcessing(): boolean {
+    return Boolean(Number(adapter.load(queueProcessingKey)));
+  }
 
-    function setQueueProcessing(isProcessing: boolean) {
-        adapter.save(queueProcessingKey, Number(isProcessing).toString());
-    }
+  function setQueueProcessing(isProcessing: boolean) {
+    adapter.save(queueProcessingKey, Number(isProcessing).toString());
+  }
 
-    function works(): boolean {
-        let works = false;
-        try {
-            adapter.save('queue-that-works', 'anything');
-            works = adapter.load('queue-that-works') === 'anything';
-            adapter.remove('queue-that-works');
-        } catch (e) {
-            console.error(e);
-        }
-        return works;
+  function works(): boolean {
+    let works = false;
+    try {
+      adapter.save('queue-that-works', 'anything');
+      works = adapter.load('queue-that-works') === 'anything';
+      adapter.remove('queue-that-works');
+    } catch (e) {
+      console.error(e);
     }
+    return works;
+  }
 
-    function reset() {
-        adapter.remove(activeQueueKey);
-        adapter.remove(backoffTimeKey);
-        adapter.remove(errorCountKey);
-        adapter.remove(queueKey);
-        adapter.remove(queueProcessingKey);
-    }
+  function reset() {
+    adapter.remove(activeQueueKey);
+    adapter.remove(backoffTimeKey);
+    adapter.remove(errorCountKey);
+    adapter.remove(queueKey);
+    adapter.remove(queueProcessingKey);
+  }
 }
 
 function save(key: string, data: string) {
-    window.localStorage.setItem(key, data);
+  window.localStorage.setItem(key, data);
 }
 
 function load(key: string): string | null {
-    return window.localStorage.getItem(key);
+  return window.localStorage.getItem(key);
 }
 
 function remove(key: string) {
-    window.localStorage.removeItem(key);
+  window.localStorage.removeItem(key);
 }
 
 function now(): number {
-    return new Date().getTime();
+  return new Date().getTime();
 }
